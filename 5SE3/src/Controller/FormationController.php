c8f4bb603eae2404b1b13ed43ba0f8e15f9fb069
ayarii <asma.ayari@esprit.tn>
2021-10-01 16:21:25 +0100

    01-10-2021 TWIG

---
+++ 5SE3/src/Controller/FormationController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FormationController extends AbstractController
+{
+    /**
+     * @Route("/formation", name="formation")
+     */
+    public function index(): Response
+    {
+        return $this->render('formation/index.html.twig', [
+            'controller_name' => 'FormationController',
+        ]);
+    }
+
+    /**
+     * @Route("/formations",name="formationAction")
+     */
+    public function listFormation()
+    {
+        //return new Response("La liste des formations");
+        $x= 10;
+        $y= "asma";
+        $formations = array(
+            array('ref' => 'form147', 'Titre' => 'Formation Symfony
+4','Description'=>'formation pratique',
+                'date_debut'=>'12/06/2020', 'date_fin'=>'19/06/2020',
+                'nb_participants'=>19),
+            array('ref'=>'form177','Titre'=>'Formation SOA' ,
+                'Description'=>'formation
+theorique','date_debut'=>'03/12/2020','date_fin'=>'10/12/2020',
+                'nb_participants'=>0),
+            array('ref'=>'form177','Titre'=>'Formation SOA' ,
+                'Description'=>'formation
+theorique','date_debut'=>'03/12/2020','date_fin'=>'10/12/2020',
+                'nb_participants'=>0),
+            array('ref'=>'form177','Titre'=>'Formation SOA' ,
+                'Description'=>'formation
+theorique','date_debut'=>'03/12/2020','date_fin'=>'10/12/2020',
+                'nb_participants'=>0),
+            array('ref'=>'form178','Titre'=>'Formation Angular' ,
+                'Description'=>'formation
+theorique','date_debut'=>'10/06/2020','date_fin'=>'14/06/2020',
+                'nb_participants'=>12));
+        return $this->render("formation/formations.html.twig",array("var1"=>$x,"var2"=>$y,"tabFormation"=>$formations));
+    }
+}

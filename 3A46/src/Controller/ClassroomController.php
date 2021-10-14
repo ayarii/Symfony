ffea1a7e7ffec16015dd8c7c27fdc47569c57274
ayarii <asma.ayari@esprit.tn>
2021-10-14 11:55:13 +0100

    séance 14-10-2021 crud classroom.

--- 3A46/src/Controller/ClassroomController.php
+++ 3A46/src/Controller/ClassroomController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+
 use App\Entity\Classroom;
+use App\Form\ClassroomType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -28,5 +31,49 @@ class ClassroomController extends AbstractController
         getRepository(Classroom::class)->findAll();
         return $this->render("classroom/list.html.twig",
             array('tabclass'=>$classrooms));
+    }
+
+    /**
+     * @Route("/add",name="addClassroom")
+     */
+    public function add(Request$request ){
+        $classroom= new Classroom();
+        $form= $this->createForm(ClassroomType::class,$classroom);
+        $form->handleRequest($request);
+        if($form->isSubmitted()){
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($classroom);
+            $em->flush();
+            return $this->redirectToRoute("listClassroom");
+        }
+        return $this->render("classroom/add.html.twig",array("formClassroom"=>$form->createView()));
+    }
+
+
+    /**
+     * @Route("/update/{id}",name="updateClassroom")
+     */
+    public function update(Request $request,$id){
+        $classroom= $this->getDoctrine()->getRepository(Classroom::class)->find($id);
+        $form= $this->createForm(ClassroomType::class,$classroom);
+        $form->handleRequest($request);
+        if($form->isSubmitted()){
+            $em = $this->getDoctrine()->getManager();
+            $em->flush();
+            return $this->redirectToRoute("listClassroom");
+        }
+        return $this->render("classroom/update.html.twig",array("formClassroom"=>$form->createView()));
+    }
+
+
+    /**
+     * @Route("/remove/{id}",name="removeClassroom")
+     */
+    public function delete($id){
+        $classroom= $this->getDoctrine()->getRepository(Classroom::class)->find($id);
+        $em= $this->getDoctrine()->getManager();
+        $em->remove($classroom);
+        $em->flush();
+        return $this->redirectToRoute("listClassroom");
     }
 }
---
+++ 3A46/src/Form/ClassroomType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Classroom;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ClassroomType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('description')
+            ->add('enabled')
+            ->add("up",SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Classroom::class,
+        ]);
+    }
+}

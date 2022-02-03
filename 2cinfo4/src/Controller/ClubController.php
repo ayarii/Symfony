b3c6622d58b045b3a88a8d6666a3838b1b827489
ayarii <asma.ayari@esprit.tn>
2022-02-03 19:39:37 +0000

    séance du 03-02-2022 rappel+function show club

---
+++ 2cinfo4/src/Controller/ClubController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Club;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ClubController extends AbstractController
+{
+    /**
+     * @Route("/club", name="club")
+     */
+    public function index(): Response
+    {
+        return $this->render('club/index.html.twig', [
+            'controller_name' => 'ClubController',
+        ]);
+    }
+
+    /**
+     * @Route("/listClub", name="listClub")
+     */
+    public function listClub(){
+        $clubs=$this->getDoctrine()->getRepository(Club::class)->findAll();
+         return $this->render("club/list.html.twig",array('tabClub'=>$clubs));
+    }
+
+    /**
+     * @Route("/showClub/{id}", name="showClub")
+     */
+    public function showClub($id){
+        $club= $this->getDoctrine()->getRepository(Club::class)->find($id);
+        return $this->render("club/show.html.twig",array('club'=>$club));
+    }
+
+}
---
+++ 2cinfo4/src/Entity/Club.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ClubRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ClubRepository::class)
+ */
+class Club
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Club;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $description;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getClub(): ?string
+    {
+        return $this->Club;
+    }
+
+    public function setClub(string $Club): self
+    {
+        $this->Club = $Club;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}

98b30bd830805f62dde601fcb24fe55b0cbb44d9
ayarii <asma.ayari@esprit.tn>
2022-02-04 15:36:47 +0000

    séance du 04-02-2022 mise en place+controller+routing

---
+++ 3A24/src/Controller/CarController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CarController extends AbstractController
+{
+    /**
+     * @Route("/car", name="car")
+     */
+    public function index(): Response
+    {
+        return $this->render('car/index.html.twig', [
+            'controller_name' => 'CarController',
+        ]);
+    }
+
+    /**
+     * @Route("/listcar", name="carlist")
+     */
+    public function listCar()
+    {
+        return new Response("3A24");
+    }
+
+}

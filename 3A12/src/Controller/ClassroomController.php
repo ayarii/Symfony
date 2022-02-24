bc71f7e985f40b6c87762e09a00c01a7fb40ef16
ayarii <asma.ayari@esprit.tn>
2022-02-24 09:31:23 +0000

    séance du 24-02-2022 Crud Classroom

--- 3A12/src/Controller/ClassroomController.php
+++ 3A12/src/Controller/ClassroomController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Classroom;
+use App\Form\ClassroomType;
+use App\Repository\ClassroomRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -27,4 +30,51 @@ class ClassroomController extends AbstractController
         $classrooms= $this->getDoctrine()->getRepository(Classroom::class)->findAll();
         return $this->render("classroom/list.html.twig",array("tabclass"=>$classrooms));
     }
+
+    /**
+     * @Route("/deleteClassroom/{id}", name="suppClassroom")
+     */
+    public function delete($id)
+    {
+        $classroom= $this->getDoctrine()->getRepository(Classroom::class)->find($id);
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($classroom);
+        $em->flush();
+        return $this->redirectToRoute("listClassroom");
+    }
+
+    /**
+     * @Route("/addClassroom", name="addClassroom")
+     */
+    public function add(Request $request)
+    {
+        $classroom= new Classroom();
+        $form= $this->createForm(ClassroomType::class,$classroom);
+        $form->handleRequest($request);
+        if($form->isSubmitted()){
+            $em= $this->getDoctrine()->getManager();
+            $em->persist($classroom);
+            $em->flush();
+            return $this->redirectToRoute("listClassroom");
+        }
+        return $this->render("classroom/add.html.twig",array("formClassroom"=>$form->createView()));
+    }
+
+    /**
+     * @Route("/updateClassroom/{id}", name="updateClassroom")
+     */
+    public function update(Request $request,ClassroomRepository $repository,$id)
+    {
+        //$classroom= $this->getDoctrine()->getRepository(Classroom::class)->find($id)
+        $classroom= $repository->find($id);
+        $form= $this->createForm(ClassroomType::class,$classroom);
+        $form->handleRequest($request);
+        if($form->isSubmitted()){
+            $em= $this->getDoctrine()->getManager();
+            $em->flush();
+            return $this->redirectToRoute("listClassroom");
+        }
+        return $this->render("classroom/update.html.twig",array("classroom"=>$classroom,"formClassroom"=>$form->createView()));
+    }
+
 }
---
+++ 3A12/src/Form/StudentType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Student;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class StudentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nce')
+            ->add('username')
+            ->add('email')
+            ->add('phoneNumber')
+            ->add('moyenne')
+
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Student::class,
+        ]);
+    }
+}

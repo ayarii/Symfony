b8601cac0cf6e832524f512193ac8a0a6bcc23d7
ayarii <asma.ayari@esprit.tn>
2022-02-25 14:23:40 +0000

    séance du 25-02-2022 suite crud student

--- 3A24/src/Controller/StudentController.php
+++ 3A24/src/Controller/StudentController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Student;
+use App\Form\StudentType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -33,12 +35,45 @@ class StudentController extends AbstractController
     public function deleteStudent($nce)
     {
         $student= $this->getDoctrine()->
-        getRepository(Student::class)->find($nce);
+         getRepository(Student::class)->find($nce);
          $em= $this->getDoctrine()->getManager();
          $em->remove($student);
          $em->flush();
          return $this->redirectToRoute("studentsList");
+    }
 
+    /**
+     * @Route("/addStudent",name="addStudent")
+     */
+    public function addStudent(Request $request)
+    {
+        $student= new Student();
+        $form= $this->createForm(StudentType::class,$student);
+        $form->handleRequest($request);
+        if($form->isSubmitted()){
+            $em= $this->getDoctrine()->getManager();
+            $em->persist($student);
+            $em->flush();
+            return $this->redirectToRoute("studentsList");
+        }
+        return $this->render("student/add.html.twig",
+            array("formStudent"=>$form->createView()));
+    }
 
+    /**
+     * @Route("/updateStudent/{nce}",name="updateStudent")
+     */
+    public function updateStudent(Request $request,$nce)
+    {
+        $student= $this->getDoctrine()->getRepository(Student::class)->find($nce);
+        $form= $this->createForm(StudentType::class,$student);
+        $form->handleRequest($request);
+        if($form->isSubmitted()){
+            $em= $this->getDoctrine()->getManager();
+            $em->flush();
+            return $this->redirectToRoute("studentsList");
+        }
+        return $this->render("student/update.html.twig",
+            array("formStudent"=>$form->createView()));
     }
 }
---
+++ 3A24/src/Form/ClassroomType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Classroom;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ClassroomType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('description')
+            ->add('enabled')
+            ->add('nbStudent')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Classroom::class,
+        ]);
+    }
+}

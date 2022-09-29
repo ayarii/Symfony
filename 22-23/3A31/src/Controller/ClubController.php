5a36210e77aaa3fde49eecb8a17465ef9e7588c7
ayarii <asma.ayari@esprit.tn>
2022-09-29 14:59:10 +0100

    29-09-2022 TWIG

---
+++ 22-23/3A31/src/Controller/ClubController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ClubController extends AbstractController
+{
+    #[Route('/club', name: 'app_club')]
+    public function index(): Response
+    {
+        return $this->render('club/index.html.twig', [
+            'controller_name' => 'ClubController',
+        ]);
+    }
+
+    #[Route('/formations', name: 'list_formation')]
+    public function formations()
+    {
+        $v1= "3A31";
+        $v2="J13";
+        $formations = array(
+            array('ref' => 'form147', 'Titre' => 'Formation Symfony
+4','Description'=>'pratique',
+                'date_debut'=>'12/06/2020', 'date_fin'=>'19/06/2020',
+                'nb_participants'=>19),
+            array('ref'=>'form177','Titre'=>'Formation SOA' ,
+                'Description'=>'theorique','date_debut'=>'03/12/2020','date_fin'=>'10/12/2020',
+                'nb_participants'=>0),
+            array('ref'=>'form178','Titre'=>'Formation Angular' ,
+                'Description'=>'theorique','date_debut'=>'10/06/2020','date_fin'=>'14/06/2020',
+                'nb_participants'=>12));
+        return $this->
+        render("club/formations.html.twig",
+        array("v1"=>$v1,"v2"=>$v2,"listFormation"=>$formations));
+    }
+
+    #[Route('/reservation', name: 'app_reservation')]
+    public function reservationFormation()
+    {
+        //return new Response("réservation!");
+        return $this->render("club/detail.html.twig");
+    }
+}

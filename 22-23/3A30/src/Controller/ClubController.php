ecfd5b777356a578a71acb12e29ce2f500723427
ayarii <asma.ayari@esprit.tn>
2022-10-10 15:33:00 +0100

    10-10-2022 Crud Club

--- 22-23/3A30/src/Controller/ClubController.php
+++ 22-23/3A30/src/Controller/ClubController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Club;
+use App\Repository\ClubRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -40,5 +43,53 @@ class ClubController extends AbstractController
     public function reservation()
     {
         return new Response("nouvelle page");
+    }
+
+    #[Route('/listClub', name: 'app_listClub')]
+    public function listClub(ClubRepository $repository)
+    {
+        #$clubs= $this->getDoctrine()->getRepository(ClubRepository::class)->findAll();
+        $clubs= $repository->findAll();
+        return $this->render("club/listClub.html.twig",
+            array("tabClub"=>$clubs));
+    }
+
+    #[Route('/addClub', name: 'app_addClub')]
+
+    public function addClub(ManagerRegistry  $doctrine)
+    {
+        $club= new Club();
+        $club->setName("club4");
+        $club->setDescription("club4");
+      #  $em= $this->getDoctrine()->getManager();
+        $em= $doctrine->getManager();
+        $em->persist($club);
+        $em->flush();
+        return $this->redirectToRoute("app_listClub");
+    }
+
+
+
+    #[Route('/updateClub/{id}', name: 'app_updateClub')]
+
+    public function updateClub($id,ManagerRegistry  $doctrine,ClubRepository  $repository)
+    {
+        $club= $repository->find($id);
+        $club->setName("clubupdate");
+        $club->setDescription("clubupdate");
+        #  $em= $this->getDoctrine()->getManager();
+        $em= $doctrine->getManager();
+        $em->flush();
+        return $this->redirectToRoute("app_listClub");
+    }
+
+    #[Route('/removeClub/{id}', name: 'app_removeClub')]
+    public function removeClub(ManagerRegistry $doctrine,ClubRepository $repository,$id)
+    {
+        $club= $repository->find($id);
+        $em= $doctrine->getManager();
+        $em->remove($club);
+        $em->flush();
+        return $this->redirectToRoute("app_listClub");
     }
 }
---
+++ 22-23/3A30/src/Entity/Club.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ClubRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ClubRepository::class)]
+class Club
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}

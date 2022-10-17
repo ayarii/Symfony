a6a3a1ec3b2f9bb7549236ebcff711116b89e2b4
ayarii <asma.ayari@esprit.tn>
2022-10-17 14:55:54 +0100

    17-10-2022 Form+jointure

--- 22-23/3A30/src/Controller/StudentController.php
+++ 22-23/3A30/src/Controller/StudentController.php
@@ -1,12 +1,38 @@
 <?php
 
-
 namespace App\Controller;
 
-
+use App\Entity\Student;
+use App\Form\StudentType;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class StudentController extends AbstractController
 {
+    #[Route('/student', name: 'app_student')]
+    public function index(): Response
+    {
+        return $this->render('student/index.html.twig', [
+            'controller_name' => 'StudentController',
+        ]);
+    }
 
+    #[Route('/addStudent', name: 'app_addstudent')]
+    public function addStudent(Request $request,ManagerRegistry $doctrine)
+    {
+        $student= new Student();
+        $form= $this->createForm(StudentType::class,$student);
+        $form->handleRequest($request);
+        if ($form->isSubmitted()){
+            $em= $doctrine->getManager();
+            $em->persist($student);
+            $em->flush();
+            return  $this->redirectToRoute("app_addstudent");
+        }
+        return $this->renderForm("student/add.html.twig",array("formStudent"=>$form));
+
+    }
 }

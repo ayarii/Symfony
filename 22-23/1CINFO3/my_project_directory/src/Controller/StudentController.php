672a344d876d9b1c77531e886ff286fb0515e8a3
ayarii <asma.ayari@esprit.tn>
2023-04-26 20:25:45 +0100

    26-04-2023 controller+twig

--- 22-23/1CINFO3/my_project_directory/src/Controller/StudentController.php
+++ 22-23/1CINFO3/my_project_directory/src/Controller/StudentController.php
@@ -22,5 +22,24 @@ class StudentController extends AbstractController
         return new Response("Bonsoir".$var);
     }
 
+    #[Route('/showStudent/{var}', name: 'show_student')]
+    public function showStudent($var)
+    {
+        $x=20;
+        $formations = array(
+            array('ref' => 'f1', 'Titre' => 'Formation Symfony
+4','Description'=>'formation pratique','nb_participants'=>19) ,
+            array('ref'=>'f2','Titre'=>'Formation SOA' ,
+                'Description'=>'formation theorique','nb_participants'=>0),
+            array('ref'=>'f3','Titre'=>'Formation Angular' ,
+                'Description'=>'formation theorique','nb_participants'=>12));
+        return $this->render
+        ("student/show.html.twig",
+            array("variable1"=>$var,"variable2"=>$x,
+               'tabFormations'=>$formations));
+    }
+
+
+
 
 }

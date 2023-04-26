3155fc263655482a0f715e799d070e914d96348d
ayarii <asma.ayari@esprit.tn>
2023-04-26 09:06:24 +0100

    25-04-2023 new controller

---
+++ 22-23/1CINFO3/my_project_directory/src/Controller/ClubController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ClubController extends AbstractController
+{
+    #[Route('/club', name: 'app_club')]
+    public function index(): Response
+    {
+        return $this->render('club/index.html.twig', [
+            'controller_name' => 'ClubController',
+        ]);
+    }
+}
---
+++ 22-23/1CINFO3/my_project_directory/src/Controller/StudentController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class StudentController extends AbstractController
+{
+    #[Route('/student', name: 'app_student')]
+    public function index(): Response
+    {
+        return $this->render('student/index.html.twig', [
+            'controller_name' => 'StudentController',
+        ]);
+    }
+
+    #[Route('/studentList/{var}', name: 'list_student')]
+    public function listStudent($var)
+    {
+        return new Response("Bonsoir".$var);
+    }
+
+
+}

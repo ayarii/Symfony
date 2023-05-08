ccb5cac17590b8a0e2c3d8d1b3491069b81d09bb
ayarii <asma.ayari@esprit.tn>
2023-05-08 20:26:57 +0100

    08-05-2023 addclassroom+deleteClassroom

--- 22-23/1CINFO3/my_project_directory/src/Controller/ClassroomController.php
+++ 22-23/1CINFO3/my_project_directory/src/Controller/ClassroomController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Classroom;
+use App\Form\ClassroomType;
 use App\Repository\ClassroomRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -31,5 +35,45 @@ class ClassroomController extends AbstractController
         $classroom = $repository->find($id);
         return $this->render("classroom/show.html.twig",array(
         'classroom'=>$classroom));
+    }
+
+    #[Route('/deleteclassroom/{id}', name: 'app_deleteclassroom')]
+    public function deleteClassroom(ManagerRegistry $doctrine,ClassroomRepository $repository,$id)
+    {
+        $classroom= $repository->find($id);
+      #  $em= $this->getDoctrine()->getManager();
+        $em=$doctrine->getManager();
+        $em->remove($classroom);
+        #$em->flush();
+        return $this->redirectToRoute("app_listclassroom");
+    }
+
+    #[Route('/addclassroom', name: 'app_addclassroom')]
+    public function addClassroom(ManagerRegistry $doctrine,Request $request)
+    {
+     /*  1ère méthode ajout statique
+      $classroom= new Classroom();
+        $classroom->setTitleClassroom("4twin6");
+        $classroom->setDescription("description");
+        $classroom->setNbrStudent("10");
+        $em= $doctrine->getManager();
+        $em->persist($classroom);
+        $em->flush();
+        return $this->redirectToRoute("app_listclassroom");*/
+       //2ème méthode (ajout avec formulaire)
+        $classroom= new Classroom();
+        $formClassroom= $this->
+        createForm(ClassroomType::class,$classroom);
+        $formClassroom->handleRequest($request);
+        if($formClassroom->isSubmitted() ){
+            $em= $doctrine->getManager();
+            $em->persist($classroom);
+            $em->flush();
+            return $this->redirectToRoute("app_listclassroom");
+        }
+        return $this->render("classroom/add.html.twig",
+            array("formlaireClassroom"=>$formClassroom->createView()));
+
+
     }
 }
---
+++ 22-23/1CINFO3/my_project_directory/src/Form/StudentType.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Student;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class StudentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstName')
+            ->add('moyenne')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Student::class,
+        ]);
+    }
+}

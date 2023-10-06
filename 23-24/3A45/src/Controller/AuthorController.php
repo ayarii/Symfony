f56e4fe0c596b575fd2feef5024172521de15531
ayarii <asma.ayari@esprit.tn>
2023-10-06 10:25:39 +0100

    06-10-2023 Twig

---
+++ 23-24/3A45/src/Controller/AuthorController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AuthorController extends AbstractController
+{
+    #[Route('/author', name: 'app_author')]
+    public function index(): Response
+    {
+        return $this->render('author/index.html.twig', [
+            'controller_name' => 'AuthorController',
+        ]);
+    }
+
+    #[Route('/list/{var}', name: 'list_author')]
+    public function listAuthor($var)
+    {
+        $authors = array(
+            array('id' => 1, 'username' => ' Victor Hugo','email'=> 'victor.hugo@gmail.com', 'nb_books'=> 100),
+            array ('id' => 2, 'username' => 'William Shakespeare','email'=>
+                'william.shakespeare@gmail.com','nb_books' => 200),
+            array('id' => 3, 'username' => ' Taha Hussein','email'=> 'taha.hussein@gmail.com','nb_books' => 300),
+        );
+
+        return $this->render("author/list.html.twig",
+            array('variable'=>$var,
+                'tabAuthors'=>$authors
+                ));
+    }
+}

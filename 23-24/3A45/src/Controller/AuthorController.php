51bd47c17f0f77cd0b3ae390b751affd334d99fa
ayarii <asma.ayari@esprit.tn>
2023-10-11 12:22:16 +0100

    11-10-23 Doctrine-Crud Author

--- 23-24/3A45/src/Controller/AuthorController.php
+++ 23-24/3A45/src/Controller/AuthorController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Author;
+use App\Repository\AuthorRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -31,4 +34,53 @@ class AuthorController extends AbstractController
                 'tabAuthors'=>$authors
                 ));
     }
+
+    #[Route('/listAuthor', name: 'authors')]
+    public function list(AuthorRepository $repository)
+    {
+        $authors = $repository->findAll();
+        return $this->render("author/listAuthors.html.twig",
+            array(
+                'tabAuthors'=>$authors
+            ));
 }
+
+    #[Route('/add', name: 'add_authors')]
+    public function addAuthor(ManagerRegistry $managerRegistry)
+    {
+        $author= new Author();
+        $author->setEmail("author6@gmail.com");
+        $author->setUsername("author6");
+       // $em= $this->getDoctrine()->getManager();
+        $em= $managerRegistry->getManager();
+        $em->persist($author);
+        $em->flush();
+        return $this->redirectToRoute("authors");
+
+   }
+
+
+    #[Route('/update/{id}', name: 'update_authors')]
+    public function updateAuthor($id,AuthorRepository $repository,ManagerRegistry $managerRegistry)
+    {
+        $author= $repository->find($id);
+        $author->setEmail("author7@gmail.com");
+        $author->setUsername("author7");
+        // $em= $this->getDoctrine()->getManager();
+        $em= $managerRegistry->getManager();
+        $em->flush();
+        return $this->redirectToRoute("authors");
+    }
+
+    #[Route('/remove/{id}', name: 'remove_authors')]
+    public function deleteAuthor(AuthorRepository $repository,$id,
+                                 ManagerRegistry $managerRegistry)
+    {
+        $author= $repository->find($id);
+        $em = $managerRegistry->getManager();
+        $em->remove($author);
+        $em->flush();
+        return $this->redirectToRoute("authors");
+    }
+}
+
---
+++ 23-24/3A45/src/Repository/AuthorRepository.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Author;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Author>
+ *
+ * @method Author|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Author|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Author[]    findAll()
+ * @method Author[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class AuthorRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Author::class);
+    }
+
+//    /**
+//     * @return Author[] Returns an array of Author objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('a')
+//            ->andWhere('a.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('a.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?Author
+//    {
+//        return $this->createQueryBuilder('a')
+//            ->andWhere('a.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}

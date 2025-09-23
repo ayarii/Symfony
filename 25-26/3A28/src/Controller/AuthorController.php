440f4a2f35e1e17c809f957b48c723e1acde095c
ayarii <asma.ayari@esprit.tn>
2025-09-23 15:37:56 +0100

    23-09-25 TWIG

---
+++ 25-26/3A28/src/Controller/AuthorController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class AuthorController extends AbstractController
+{
+    #[Route('/author', name: 'app_author')]
+    public function index(): Response
+    {
+        return $this->render('author/index.html.twig', [
+            'controller_name' => 'AuthorController',
+        ]);
+    }
+
+    #[Route('/list', name: 'list_author')]
+    public function listAuthors()
+    {   $var = "3A28";
+        $authors = array(
+            array('id' => 1, 'picture' => '/images/Victor-Hugo.jpg','username' => 'Victor Hugo', 'email' =>
+                'victor.hugo@gmail.com ', 'nb_books' => 100),
+            array('id' => 2, 'picture' => '/images/william-shakespeare.jpg','username' => ' William Shakespeare', 'email' =>
+                ' william.shakespeare@gmail.com', 'nb_books' => 200 ),
+            array('id' => 3, 'picture' => '/images/Taha_Hussein.jpg','username' => 'Taha Hussein', 'email' =>
+                'taha.hussein@gmail.com', 'nb_books' => 300),
+        );
+        return $this->render("author/list.html.twig",
+            array('x'=>$var,'tabAuthors'=>$authors));
+    }
+}
